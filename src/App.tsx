cbb6a4204e9e75ed3f7948a9fef02fbba863295b
gayoung <gayoungyeom@gmail.com>
2023-03-21 22:43:21 +0900

    add: theme 타입 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import { FormEvent, useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState('');
-
-  const onChange = (event: FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(`hello ${value}`);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hi</H1>
+    </Container>
   );
 }
 
