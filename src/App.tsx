3d66629e10b912f9a898f37be10eb38b2a5075cd
gayoung <gayoungyeom@gmail.com>
2023-03-21 22:28:20 +0900

    add: event 타입 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import styled, { keyframes } from 'styled-components';
-import Circle from './Circle';
+import { FormEvent, useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const onChange = (event: FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`hello ${value}`);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black" />
-      <Circle bgColor="tomato" text="Im tomato" />
-      <Circle bgColor="yellow" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
