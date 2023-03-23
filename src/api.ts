504e380db1711ec57409637b3e88e8700871d39d
gayoung <gayoungyeom@gmail.com>
2023-03-23 23:37:25 +0900

    feat: Chart 페이지 쿼리 생성

--- src/api.ts
+++ src/api.ts
@@ -14,3 +14,13 @@ export const fetchCoinPrice = async (coinId: string) => {
   const response = await fetch(`${BASE_URL}/tickers/${coinId}`);
   return await response.json();
 };
+
+export const fetchCoinHistory = async (coinId: string) => {
+  //2 weeks of data
+  //   const endDate = Math.floor(Date.now() / 1000);
+  //   const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  const response = await fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+  );
+  return await response.json();
+};
