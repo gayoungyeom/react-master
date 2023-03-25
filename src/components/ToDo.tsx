2dace5a43e8f56be8abbcacedc43388f86709ef6
gayoung <gayoungyeom@gmail.com>
2023-03-25 17:39:58 +0900

    feat: ToDoList의 순서 그대로 교체

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,45 @@
-import { ITodo } from '../atoms';
+import { MouseEvent } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { ITodo, toDoState } from '../atoms';
 
-function ToDo({ text }: ITodo) {
+function ToDo({ id, text, category }: ITodo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = e;
+
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldTodo = oldToDos[targetIndex];
+      const newTodo = { ...oldTodo, category: name as ITodo['category'] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newTodo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>TODO</button>
-      <button>DONE</button>
+      {category !== 'DOING' && (
+        <button name="DOING" onClick={onClick}>
+          DOING
+        </button>
+      )}
+      {category !== 'TODO' && (
+        <button name="TODO" onClick={onClick}>
+          TODO
+        </button>
+      )}
+      {category !== 'DONE' && (
+        <button name="DONE" onClick={onClick}>
+          DONE
+        </button>
+      )}
     </li>
   );
 }
