34ba4e849f8231480f409380d22a58df57dd8cab
gayoung <gayoungyeom@gmail.com>
2023-04-24 16:22:35 +0900

    feat: 보드 간 이동 기능 추가

--- src/DnDTest.tsx
+++ src/DnDTest.tsx
@@ -26,15 +26,38 @@ const Boards = styled.div`
 const DnDTest = () => {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+  const onDragEnd = (info: DropResult) => {
+    console.log(info);
+    const { destination, source, draggableId } = info;
     if (!destination) return;
 
-    // setToDos((oldTodos) => {
-    //   const copyToDos = [...oldTodos];
-    //   copyToDos.splice(source.index, 1); //1. 이동 전 위치에서 아이템 제거
-    //   copyToDos.splice(destination?.index, 0, draggableId); //2. 이동 후 위치에 아이템 추가
-    //   return copyToDos;
-    // });
+    if (destination.droppableId === source.droppableId) {
+      //same board movement
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1); //1. 이동 전 위치에서 아이템 제거
+        boardCopy.splice(destination?.index, 0, draggableId); //2. 이동 후 위치에 아이템 추가
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    } else {
+      //cross board movement
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        sourceBoard.splice(source.index, 1);
+
+        const destinBoard = [...allBoards[destination.droppableId]];
+        destinBoard.splice(destination?.index, 0, draggableId);
+
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinBoard,
+        };
+      });
+    }
   };
 
   return (
