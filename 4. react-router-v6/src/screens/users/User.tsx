17043b1e2363cd4f96da5020908a5a472225d2e1
gayoung <gayoungyeom@gmail.com>
2023-05-04 13:38:57 +0900

    feat: Outlet

--- 4. react-router-v6/src/screens/users/User.tsx
+++ 4. react-router-v6/src/screens/users/User.tsx
@@ -1,13 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import { users } from '../../db';
 
 const User = () => {
   const { userId } = useParams();
 
   return (
-    <h1>
-      User with ID {userId} is named: {users[Number(userId) - 1].name}
-    </h1>
+    <div>
+      <h1>
+        User with ID {userId} is named: {users[Number(userId) - 1].name}
+      </h1>
+      <hr />
+      <Link to="followers">See followers</Link>
+      <Outlet />
+      <p>Outlet을 렌더링해야 followers가 화면에 렌더링된다.</p>
+    </div>
   );
 };
 
